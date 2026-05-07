1f8ad705ad4713e1b938c26a70024b55aa8e74bd
aleksey.by2007@gmail.com <aleksey.by2007@gmail.com>
2026-05-07 10:12:58 +0300

    add commit

--- demo/src/main/java/com/example/demo/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.exception.ServiceException;
-
-public interface EmailService {
-
-    void sendConfirmationEmail(String toEmail, String confirmationToken) throws ServiceException;
-}
